45a53bfedc18b369597a11dc1f25906f6b39ff1d
Arghyadip Chakraborty <arghyadip.chak16@gmail.com>
2020-12-10 10:52:33 +0530

    Final Update

---
+++ codes/plotSpectra.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.signal import detrend
+
+data1 = pd.read_excel('Data.xlsx')
+X = data1.drop(columns=['Num of soil Sample'])
+
+X1 = detrend(X)
+
+wl = list(X.columns.values)
+with plt.style.context(('ggplot')):
+    plt.plot([1000, 2500], [0, 0], color='black', linewidth=1)
+    plt.plot(wl, X1.T)
+    plt.xlabel('Wavelength (nm)')
+    plt.ylabel('Diffuse Reflectance')
+plt.show()
